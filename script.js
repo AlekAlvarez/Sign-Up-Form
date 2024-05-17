83a08eb1181cd0138563a28c0fb48b4c20912843
Aleksander Alvarez <alekalvarez123@gmail.com>
2024-05-16 23:49:11 -0500

    Completes javascript

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const password=document.querySelector("#password");
 const passwordConfirm=document.querySelector("#password-confirm");
 const invalid=document.createElement("div");
 const form=document.querySelector("form");
+const create=document.querySelector("#create-account");
+create.addEventListener("click",()=>{
+    form.submit();
+});
 invalid.innerText="Passwords must match";
 invalid.style.color="red";
 invalid.style.fontSize="xsmall";
